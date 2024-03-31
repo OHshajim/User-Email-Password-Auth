b1ca3f893c06a2ed8752564a67fbb0d039370c03
OHshajim <ajshajimmax@gmail.com>
2024-04-01 02:09:28 +0600

    show icon and charector condition

--- src/assets/Components/SIgnup/Signup.jsx
+++ src/assets/Components/SIgnup/Signup.jsx
@@ -1,10 +1,13 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../../Firebase/Firebase.config";
 import { useState } from "react";
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Signup = () => {
     const [error, setError] = useState('')
     const [success, setSuccess] = useState('')
+    const [isShow, setShow] = useState(false)
     const handleSignUp = e => {
         e.preventDefault();
         const email = e.target.email.value
@@ -13,9 +16,13 @@ const Signup = () => {
         // reset error
         setError(' ')
         setSuccess(' ')
-        if(password.length<6){
+        if (password.length < 6) {
             setError("Password should be at least 6 characters ")
-            return ;
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            setError("Password should be have  Capital  letter ")
+            return;
         }
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
@@ -34,7 +41,16 @@ const Signup = () => {
                 <h2 className='text-3xl mb-5'>Please Sign up</h2>
                 <form className="space-y-4 " onSubmit={handleSignUp}>
                     <input className="w-3/4 py-3 px-3 text-xl" required placeholder="Enter your Email" type="email" name="email" /><br />
-                    <input className="w-3/4 py-3 px-3 text-xl" required placeholder="Enter your Password" type="password" name="password" /><br />
+                    <div className="relative">
+                        <input
+                            className="w-3/4 py-3 px-3 text-xl"
+                            required placeholder="Enter your Password"
+                            type={isShow ? "text" : "password"} name="password"
+                        />
+                        <span className="absolute top-4 text-2xl xl:right-32 right-14 sm:right-20 " onClick={() => setShow(!isShow)}>{isShow ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}</span>
+                    </div>
+
+                    <br />
                     <input className="btn btn-accent w-3/4" type="submit" name="submit" />
                 </form>
                 {
